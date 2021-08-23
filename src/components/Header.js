3dacc688b8f9ef29d260530ac1edcf1dab9746fd
Thiago <thiago_a580@outlook.com>
2021-08-23 18:24:34 -0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a hred="/">
+                    <img src="https://mundonegro.inf.br/wp-content/uploads/2021/04/580b57fcd9996e24bc43c529.png" alt="Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Usuário" />
+                </a>
+            </div>
+        </header>
+    );
+}
